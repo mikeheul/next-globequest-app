4546046f444d1bbddeca10471bf680becb6307d4
Micka <mickael.murmann@gmail.com>
2024-06-17 17:04:22 +0200

    Hover WebsiteLink component

--- components/WebsiteLink.tsx
+++ components/WebsiteLink.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const WebsiteLink: React.FC<Props> = ({ poi }) => {
     return (
-        <div className='inline-flex gap-2 px-3 py-2 my-3 text-white rounded-sm bg-[#F7775E]'>
+        <div className='inline-flex gap-2 px-3 py-2 my-3 text-white rounded-sm bg-[#F7775E] hover:bg-[#ec6b54]'>
             <Globe2Icon className='h-5 w-5' />
             <a className='no-underline' target='_blank' rel='noopener noreferrer' href={poi.website}>
                 {poi.name} website

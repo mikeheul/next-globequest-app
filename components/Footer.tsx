c7f6bff76275004a8a41d15b6fd7b162e313588e
Micka <mickael.murmann@gmail.com>
2024-06-17 17:07:29 +0200

    Update footer component (copyright + current year)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const Footer = () => {
+
+    const currentYear = new Date().getFullYear();
+
     return (
         <div className='flex flex-col justify-center items-center min-h-[200px] w-full bg-[#1D1D1D] text-white'>
-            Footer
+            &copy;{currentYear} GlobeQuest
         </div>
     )
 }

03ead4da52fc6ce2b35c2b5c1dd1e5ff5b5295e7
Micka <mickael.murmann@gmail.com>
2024-06-18 12:32:56 +0200

    SVG icon for Map marker

--- components/Map.tsx
+++ components/Map.tsx
@@ -5,6 +5,7 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { LatLngExpression, LatLngTuple, Marker as LeafletMarker } from 'leaflet';
 import { MarkerMuster } from "react-leaflet-muster";
 import { divIcon } from 'leaflet';
+import { LeafyGreenIcon } from 'lucide-react';
 
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
@@ -20,6 +21,16 @@ interface MapProps {
 const defaults = {
     zoom: 17,
 }
+
+const customIcon = divIcon({
+    html: `
+    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-pin"><path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z"/><circle cx="12" cy="10" r="3"/></svg>
+    `,
+    iconSize: [30, 30],
+    iconAnchor: [30, 39],
+    popupAnchor: [-5, -30],
+    className: 'text-[#F7775E]'
+});
 
 const Map = (Map: MapProps) => {
     const { zoom = defaults.zoom, posix, poiName, address } = Map
@@ -54,13 +65,15 @@ const Map = (Map: MapProps) => {
                 <Marker 
                     position={posix} 
                     draggable={false}
-                    icon={divIcon({
-                        iconSize: [30, 30],
-                        iconAnchor: [30, 39],
-                        popupAnchor: [-5, -30],
-                        className: "text-4xl",
-                        html: "🌟",
-                    })}
+                    // icon={divIcon({
+                    //     iconSize: [30, 30],
+                    //     iconAnchor: [30, 39],
+                    //     popupAnchor: [-5, -30],
+                    //     className: "text-4xl",
+                    //     // html: "💀",
+                    //     // html: "<p class='w-[200px] text-center text-sm text-white bg-red-500 border-slate-200 px-2 py-1'>Kevin was here</p>",
+                    // })}
+                    icon={customIcon}
                     ref={markerRef}
                 >
                     <Popup>

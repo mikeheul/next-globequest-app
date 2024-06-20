d04019c8df2aa203c11d90f7edb8027c064c88e3
Micka <mickael.murmann@gmail.com>
2024-06-20 11:55:50 +0200

    Custom itineraries page

--- app/itinerary/[itineraryId]/page.tsx
+++ app/itinerary/[itineraryId]/page.tsx
@@ -1,6 +1,7 @@
 import MapMulti from '@/components/MapMulti';
 import MapRoute from '@/components/MapRoute';
 import { db } from '@/lib/db'
+import { RouteIcon } from 'lucide-react';
 import React from 'react'
 
 const ItineraryPage = async ({ params }: { params: { itineraryId: string }}) => {
@@ -35,8 +36,11 @@ const ItineraryPage = async ({ params }: { params: { itineraryId: string }}) => 
     return (
         <>
             <div className='p-10'>
-                <h1 className='text-3xl font-medium mb-5'>{ itinerary?.name }</h1>
-                <div className='flex flex-col gap-2 mb-3'>
+                <h1 className='text-3xl font-bold mb-5'>{itinerary?.name}</h1>
+                <p className='italic text-slate-700'>{itinerary?.description}</p>
+
+                <h2 className='flex flex-row items-center text-2xl my-5 font-medium'><RouteIcon className='mr-4' /> Itinerary details</h2>
+                <div className='flex flex-col gap-2 my-10'>
                     {pois.map((poi, index) => (
                         <p key={index} className='flex items-center gap-4'><span className='flex justify-center items-center p-4 bg-[#F7775E] text-white text-sm h-[10px] w-[10px] rounded-full'>{index+1}</span> {poi.poiName}</p>
                     ))}

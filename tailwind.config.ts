751e62441a7d205f9913d81037d50b6f2e73119d
Micka <mickael.murmann@gmail.com>
2024-06-20 16:04:01 +0200

    Test animation-scale (ko)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,15 +67,15 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
-        spin: {
-          '0%': { transform: 'rotate(0deg)' },
-          '100%': { transform: 'rotate(360deg)' },
+        sczle: {
+          '0%': { transform: 'scale(0)' },
+          '100%': { transform: 'scale(1.1)' },
         },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        "spin": "spin 1s linear infinite",
+        "scale": "spin 1s linear infinite",
       },
     },
   },

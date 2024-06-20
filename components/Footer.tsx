aff7dc54a692990cb799912c3b8739ae07d7d1e7
Micka <mickael.murmann@gmail.com>
2024-06-21 01:14:46 +0200

    Fix css

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     const currentYear = new Date().getFullYear();
 
     return (
-        <div className='flex flex-col justify-center items-center min-h-[150px] w-full bg-[#1D1D1D] text-white'>
+        <div className='flex flex-col justify-center items-center min-h-[150px] w-full bg-[#1D1D1D] text-slate-500'>
             &copy;{currentYear} GlobeQuest
         </div>
     )

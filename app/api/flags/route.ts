696c9c22dd4805b3c7f9c2592d087a3ab6efb181
Micka <mickael.murmann@gmail.com>
2024-06-24 14:14:07 +0200

    Test subregion (ko)

--- app/api/flags/route.ts
+++ app/api/flags/route.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-    
     const { searchParams } = new URL(req.url);
     const countryName = searchParams.get('countryName');
 

792d0ff00ac543fa418bd4f09b357d46652f0e29
Micka <mickael.murmann@gmail.com>
2024-06-25 21:57:48 +0200

    Switcher dark / light mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="">
       <body className={`${inter.variable} ${permanent.variable} ${luckiest.variable} font-normal`}>
       {/* <body className={inter.variable}> */}
         <div className='flex flex-col h-screen'>
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -11,6 +11,7 @@ const Navbar = () => {
     const [isOpen, setIsOpen] = React.useState(false);
     const [activePath, setActivePath] = useState('');
     const [previousPath, setPreviousPath] = useState('');
+    const [theme, setTheme] = useState('light');
 
     // Define an array of link objects to manage navigation links
     const links = [
@@ -23,12 +24,23 @@ const Navbar = () => {
 
     useEffect(() => {
         setActivePath(window.location.pathname);
+        const savedTheme = localStorage.getItem('theme') || 'light';
+        setTheme(savedTheme);
+        document.documentElement.classList.add(savedTheme);
     }, []);
 
     const handleLinkClick = (href: string) => {
         setPreviousPath(activePath); // Set the previous active path
         setActivePath(href); // Set the new active path
         setIsOpen(false); // Close the menu on mobile
+    };
+
+    const toggleTheme = () => {
+        const newTheme = theme === 'light' ? 'dark' : 'light';
+        setTheme(newTheme);
+        localStorage.setItem('theme', newTheme);
+        document.documentElement.classList.remove(theme);
+        document.documentElement.classList.add(newTheme);
     };
 
     return (
@@ -41,11 +53,12 @@ const Navbar = () => {
                     {/* Left side of the navigation bar */}
                     <div className="flex">
                         {/* Brand/logo link */}
-                        <div className="flex justify-center items-center">
+                        <div className="flex justify-center items-center mr-5">
                             <a href="/home" className="text-sm sm:text-xl md:text-2xl font-bold font-permanent text-gray-700 dark:text-white">GLOBE QUEST</a>
                         </div>
                         {/* Desktop navigation links */}
-                        <div className="hidden sm:ml-6 sm:flex sm:items-center sm:space-x-8">
+                        <div className="hidden sm:ml-6 sm:flex sm:justify-between sm:items-center sm:space-x-8">
+                            <>
                             {links.map((link) => (
                                 <Link 
                                     key={link.href} 
@@ -56,6 +69,13 @@ const Navbar = () => {
                                     {link.label}
                                 </Link>
                             ))}
+                            </>
+                            <button
+                                onClick={toggleTheme}
+                                className="hidden text-2xl absolute lg:block right-10 top-6 cursor-pointer text-gray-500 dark:text-white mr-4"
+                            >
+                                {theme === 'light' ? '🌙' : '☀️'}
+                            </button>
                         </div>
                     </div>
                     {/* Mobile menu button */}
@@ -82,15 +102,23 @@ const Navbar = () => {
             >
                 <div className="px-8 pt-2 pb-3 space-y-1">
                     {links.map((link) => (
-                        <Link 
-                            key={link.href} 
-                            href={link.href} 
-                            onClick={() => handleLinkClick(link.href)}
-                            className={`text-gray-500 block px-3 py-2 rounded-md text-base font-medium ${activePath === link.href ? 'text-white font-bold bg-slate-600 px-3' : ''}`}
-                        >
-                            {link.label}
-                        </Link>
+                        <>
+                            <Link 
+                                key={link.href} 
+                                href={link.href} 
+                                onClick={() => handleLinkClick(link.href)}
+                                className={`text-gray-500 block px-3 py-2 rounded-md text-base font-medium ${activePath === link.href ? 'text-white font-bold bg-slate-600 px-3' : ''}`}
+                            >
+                                {link.label}
+                            </Link>
+                        </>
                     ))}
+                    <button
+                        onClick={toggleTheme}
+                        className="px-3 py-2 top-0 text-gray-500 dark:text-white mr-4"
+                    >
+                        {theme === 'light' ? '🌙' : '☀️'}
+                    </button>
                 </div>
             </div>
         </nav>

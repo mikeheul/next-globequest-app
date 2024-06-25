60504703860f1057fbe888f96b6b59adfc402c3e
Micka <mickael.murmann@gmail.com>
2024-06-25 14:39:50 +0200

    Fix CSS dark mode contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,7 +7,7 @@ const ContactPage = () => {
     return (
         <div className='relative h-screen flex flex-col justify-center items-center bg-slate-800'>
             <Image src={bg} alt='' className='w-full h-full object-cover' />
-            <div className='absolute h-full w-full bg-gradient-to-t from-[#1D1D1D] from-10% via-slate-900/50 to-[#1D1D1D] to-90%'></div>
+            <div className='absolute h-full w-full bg-gradient-to-t from-[#1D1D1D] via-slate-900/50 to-[#1D1D1D] dark:bg-gradient-to-t dark:from-slate-900 dark:via-slate-900/50 dark:to-slate-900'></div>
             <div className='absolute flex flex-col gap-8 justify-center items-center max-w-[80%] md:max-w-[50%] text-white text-center'>
                 <h1 className='text-3xl md:text-7xl font-light uppercase'>Contact</h1>
                 <MinusIcon />
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     const currentYear = new Date().getFullYear();
 
     return (
-        <div className='flex flex-col justify-center items-center min-h-[150px] w-full bg-[#1D1D1D] text-slate-300'>
+        <div className='flex flex-col justify-center items-center min-h-[150px] w-full bg-[#1D1D1D] text-slate-300 dark:bg-slate-900'>
             &copy;{currentYear} GlobeQuest
         </div>
     )

78c006fd385cf53d51af9e8d71dbf83658c2ab7b
Micka <mickael.murmann@gmail.com>
2024-06-26 21:21:41 +0200

    Fix CSS Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
     return (
         <div className='flex flex-col justify-center items-center min-h-[150px] w-full bg-[#1D1D1D] text-slate-300 dark:bg-slate-900'>
             &copy;{currentYear} GlobeQuest
-            <div><a className='underline font-semibold text-marker' href='/upload'>Upload file on Cloudinary</a></div>
+            <div><a className='hover:underline font-semibold text-marker' href='/upload'>Upload file on Cloudinary</a></div>
         </div>
     )
 }
